3aa435994c8a8f12939075ff51023ae76f871634
Haley Someone <hbarlar1@gmail.com>
2018-08-01 19:31:08 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
   
-  songs.each_with_index do |index, song|
+  songs.each_with_index do |song, index|
   if input.to_i == (index + 1) || input == song
     puts "Playing #{songs[song]}"
   
